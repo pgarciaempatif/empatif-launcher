159490bc2ec93bcc7b768e7fb0c12ce98df1f977
Raul Hinojosa <rhinojosa@empatif.com>
2026-01-26 15:56:40 +0100

    Cambios de estilos y funcionamiendo de recursos y guias en info

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { FEATURED_LINK, HOME_LINKS } from '../config/app-links';
 import { environment } from 'src/environments/environment';
 import { HomeLink } from '../shared/models/types.model';
 import { TranslateModule } from '@ngx-translate/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -14,6 +15,7 @@ import { TranslateModule } from '@ngx-translate/core';
   imports: [CommonModule, IonicModule, TranslateModule],
 })
 export class HomePage implements OnInit {
+  private router = inject(Router);
   title = environment.appTitle;
   featuredLink: HomeLink | null = FEATURED_LINK;
   tileLinks: HomeLink[] = HOME_LINKS;
@@ -26,6 +28,10 @@ export class HomePage implements OnInit {
   }
 
   openExternal(url: string) {
-    window.open(url, '_blank');
+    if (url.startsWith('http')) {
+      window.open(url, '_blank');
+    } else {
+      this.router.navigate([url]);
+    }
   }
 }
